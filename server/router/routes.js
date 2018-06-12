a1f94860a56d1b9b194eb94db7bfa5698e3a6085
xilin <cuzzcool123@gmail.com>
2018-06-13 00:04:30 +0800

    Init router

---
+++ server/router/routes.js
@@ -0,0 +1,13 @@
+const Router = require('koa-router')
+const router = new Router()
+const logger = require('../logger')
+
+// 获取某个labelId对应的文章
+/**
+ * @param query labelid
+ */
+router.get('/label', async (ctx) => {
+ 
+})
+
+module.exports = router
