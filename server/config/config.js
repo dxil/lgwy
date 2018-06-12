f80f4d7c95f3e1d26934d3e58e071ca544a137ee
xilin <cuzzcool123@gmail.com>
2018-06-13 00:04:46 +0800

    add mongodb config

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,9 @@ module.exports = {
   dev: {
     koaConfig: {
       port: 3333
+    },
+    mongodb: {
+      url: 'mongodb://127.0.0.1:27017/lgwy'
     }
   }
 }
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const Koa = require('koa')
 const json = require('koa-json')
 const bodyParser = require('koa-bodyparser')
 const cors = require('@koa/cors')
+const mongoose = require('mongoose')
 const router = require('./router/routes')
 const config = require('./config/config').dev
 // const DB = require('../db')
@@ -16,20 +17,21 @@ const logger = require('./logger')
 
 const server = new Koa()
 
-module.exports = {
-  start: (port, natsdClient) => {
-    port = port || config.koaConfig.port
-    // 错误日志和访问日志
-    // server.use(log4js.koaLogger(apiServerLog, {
-    //   level: 'auto'
-    // }))
+const port = config.koaConfig.port
+// 错误日志和访问日志
+// server.use(log4js.koaLogger(apiServerLog, {
+//   level: 'auto'
+// }))
 
-    server.use(bodyParser())
-    server.use(json()).use(cors())
+server.use(bodyParser())
+server.use(json()).use(cors())
+server.use(router.routes()).use(router.allowedMethods())
 
-    server.use(router.routes()).use(router.allowedMethods())
-    server.listen(port, () => {
-      logger.info(`koa-server listenning at port: ${port}`)
-    })
-  }
-}
+server.listen(port, () => {
+  logger.info(`koa-server listenning at port: ${port}`)
+})
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongodb.url, () => {
+  logger.info(`mongodb listenning at url: ${config.mongodb.url}`)
+})
