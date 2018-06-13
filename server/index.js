8bed50cb035942200d5a3d91fe5f5857555fd79a
xilin <cuzzcool123@gmail.com>
2018-06-14 00:02:03 +0800

    Add  log4js middleware for koa-logger

--- server/index.js
+++ server/index.js
@@ -11,17 +11,17 @@ const cors = require('@koa/cors')
 const mongoose = require('mongoose')
 const router = require('./router/routes')
 const config = require('./libs/config').get('server')
+const log4js = require('./libs/log')
 // const DB = require('../db')
-// const log4js = require('../../libs/log')
 const serverLog = require('./logger')
 
 const server = new Koa()
 
 const port = config.koaConfig.port
 // 错误日志和访问日志
-// server.use(log4js.koaLogger(apiServerLog, {
-//   level: 'auto'
-// }))
+server.use(log4js.koaLogger(serverLog, {
+  level: 'auto'
+}))
 
 server.use(bodyParser())
 server.use(json()).use(cors())
