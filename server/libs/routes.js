21859c3092d31e008b4c1a35b65c6b54c4017df7
xilin <cuzzcool123@gmail.com>
2018-10-18 15:57:45 +0800

    Update: add error response

--- server/libs/routes.js
+++ server/libs/routes.js
@@ -12,6 +12,7 @@ router.use(function (ctx, next) {
 
 // router.use('/', require('../routes/index')) // 暂无首页
 router.use('/label', require('../routes/label'))
+router.use('/user', require('../routes/user'))
 // router.use('/case', require('../routes/case'))
 // router.use('/admin', require('../routes/admin')) // todo 后台统计
 
@@ -20,12 +21,26 @@ router.use(function (ctx, next) {
   // const _user = ctx.session.user
 
   // ctx.state.user = _user
+
+  let error = ctx._error
+  if (error) {
+    ctx.body = {
+      code: error.code, // 0 成功 -1 参数错误 -2 内部错误
+      msg: 'error',
+      errMsg: error.msg,
+      data: {},
+      time: new Date()
+    }
+    return next()
+  }
+
   ctx.body = {
     code: 0, // 0 成功 -1 参数错误 -2 内部错误
     msg: 'success',
     data: ctx._result,
     time: new Date()
   }
+
   return next()
 })
 module.exports = router
